bd8b79c6825b8c1261beea152e3b69fa08bb0a47
Ike Njoku <ikeenjoku@gmail.com>
2020-02-22 11:12:20 +0100

    create game hook

--- src/App.js
+++ src/App.js
@@ -1,47 +1,17 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React from 'react'
+import useSpeedTyper from './hooks/useSpeedTyper'
 import './App.css';
 
-const TIME_REMAINING = 10
 function App() {
-  const [wordCount, setWordCount] = useState(0)
-  const [gameIsRunning, setGameisRunning] = useState(false)
-  const [textAreaContent, setTextAreaContent] = useState('')
-  const [timeRemaining, setTimeRemaining] = useState(TIME_REMAINING)
-  const textAreaRef = useRef(null)
-
-  const countWords = (str) => {
-    const count = str ? str.match(/(\w+)/g).length : 0
-    setWordCount(count)
-  }
-
-  const startGame = () => {
-    setTimeRemaining(TIME_REMAINING)
-    setTextAreaContent('')
-    setWordCount(0)
-    setGameisRunning(true)
-    console.log('textAreaRef', textAreaRef)
-    textAreaRef.current.disabled = false
-    textAreaRef.current.focus()
-  }
-
-  const endGame = () => {
-    setGameisRunning(false)
-    countWords(textAreaContent)
-  }
-
-  useEffect(() => {
-    if (timeRemaining > 0 && gameIsRunning) {
-      const timerId = setTimeout(() => {
-        const newTimeRemaining = timeRemaining - 1
-        setTimeRemaining(newTimeRemaining)
-      }, 1000)
-      return () => {
-        clearTimeout(timerId)
-      }
-    } else if (timeRemaining === 0){
-      endGame()
-    }
-  }, [timeRemaining, gameIsRunning])
+  const {
+     textAreaRef,
+     gameIsRunning,
+     textAreaContent,
+     setTextAreaContent,
+     timeRemaining,
+     startGame,
+     wordCount
+    } = useSpeedTyper(13)
 
   return (
     <div>
