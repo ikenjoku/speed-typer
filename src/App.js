701b54fe824f99a4efeb3942020a85e339d48140
Ike Njoku <ikeenjoku@gmail.com>
2020-02-22 10:30:35 +0100

    create counter component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
 
+const TIME_REMAINING = 10
 function App() {
+  const [wordCount, setWordCount] = useState(0)
+  const [gameIsRunning, setGameisRunning] = useState(false)
+  const [textAreaContent, setTextAreaContent] = useState('')
+  const [timeRemaining, setTimeRemaining] = useState(TIME_REMAINING)
+  const textAreaRef = useRef(null)
+
+  const countWords = (str) => {
+    const count = str ? str.match(/(\w+)/g).length : 0
+    setWordCount(count)
+  }
+
+  const startGame = () => {
+    setTimeRemaining(TIME_REMAINING)
+    setTextAreaContent('')
+    setWordCount(0)
+    setGameisRunning(true)
+    console.log('textAreaRef', textAreaRef)
+    textAreaRef.current.disabled = false
+    textAreaRef.current.focus()
+  }
+
+  const endGame = () => {
+    setGameisRunning(false)
+    countWords(textAreaContent)
+  }
+
+  useEffect(() => {
+    if (timeRemaining > 0 && gameIsRunning) {
+      const timerId = setTimeout(() => {
+        const newTimeRemaining = timeRemaining - 1
+        setTimeRemaining(newTimeRemaining)
+      }, 1000)
+      return () => {
+        clearTimeout(timerId)
+      }
+    } else if (timeRemaining === 0){
+      endGame()
+    }
+  }, [timeRemaining, gameIsRunning])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>SPEED-TYPER</h1>
+      <textarea
+        ref={textAreaRef}
+        disabled={!gameIsRunning}
+        value={textAreaContent}
+        onChange={e => setTextAreaContent(e.target.value)}
+      />
+      <h4>Time remaining: {timeRemaining}</h4>
+      <button onClick={startGame}>Start Game</button>
+      <h1>Word count: {wordCount}</h1>
     </div>
-  );
+  )
 }
 
 export default App;
